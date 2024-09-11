c3cabd9b3a7d68404650f5559e2d51732a220eed
sloraris <97694636+sloraris@users.noreply.github.com>
2024-09-11 11:35:58 -0600

    created practice directory and renamed practice.py to classes.py

---
+++ practice/classes.py
@@ -0,0 +1,27 @@
+from collections import namedtuple
+from dataclasses import dataclass
+
+a = ['Bob', 'Zimmerman', 72]
+d = {'first_name': 'Bob', 'last_name': 'Zimmerman', 'age': 83}
+
+def greet(d):
+    print(f"Hello, my name is {d['first_name']} and I'm {d['age']} years old.")
+
+greet(d)
+
+@dataclass
+class Student:
+    first: str
+    last: str
+    age: int
+
+    def __str__(self):
+        return f"Hello, my name is {self.first} {self.last} and I'm {self.age} years old."
+
+    def are_you_older_than_100(self):
+        return self.age > 100
+
+
+bob = Student('Bob', 'Zimmerman', 83)
+
+print(bob.are_you_older_than_100())
