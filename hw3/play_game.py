fd8b7ba43b7d88f10a6f54c733533a25c2cc645d
sloraris <97694636+sloraris@users.noreply.github.com>
2024-09-13 21:18:59 -0600

    updated formatting to meet assignment requirements, added super extra formatting

--- hw3/play_game.py
+++ hw3/play_game.py
@@ -6,6 +6,7 @@ RED = "\033[31m"
 GREEN = "\033[32m"
 YELLOW = "\033[33m"
 CYAN = "\033[36m"
+MAGENTA = "\033[35m"
 RESET = "\033[0m"
 
 
@@ -21,13 +22,13 @@ def score_keeper(score, card):
 def did_you_beat_the_dealer(score, dealer_score):
     if score > dealer_score:
         print(f"{GREEN}The dealer's score is:{RESET}", dealer_score)
-        print(snark.win_dealer_under())
+        print(f"{MAGENTA}{snark.win_dealer_under()}{RESET}")
     elif dealer_score > 21:
         print(f"{GREEN}The dealer's score is:{RESET}", dealer_score)
-        print(snark.win_dealer_bust())
+        print(f"{MAGENTA}{snark.win_dealer_bust()}{RESET}")
     elif score < dealer_score and dealer_score <= 21:
         print(f"{RED}The dealer's score is:{RESET}", dealer_score)
-        print(snark.lose_dealer())
+        print(f"{MAGENTA}{snark.lose_dealer()}{RESET}")
     elif score == dealer_score:
         print(f"{YELLOW}well that's awkward, you and the dealer both got:{RESET}", score)
     else:
@@ -38,13 +39,13 @@ def did_you_beat_the_dealer(score, dealer_score):
 # game_over, player_won
 def game_status_checker(score, hit_number):
     if score > 21:
-        print(snark.hit_bust())
+        print(f"{MAGENTA}{snark.hit_bust()}{RESET}")
         return True, False
     elif score == 21:
-        print(snark.blackjack())
+        print(f"{MAGENTA}{snark.blackjack()}{RESET}")
         return True, True
     elif score < 21 and hit_number > 0:
-        print(snark.hit_success())
+        print(f"{MAGENTA}{snark.hit_success()}{RESET}")
         return False, None
     else:
         return False, None
@@ -81,20 +82,27 @@ snark = Snark()
 
 
 # main game loop
+print("Welcome to Snarkjack")
 while True:
     # create a new deck of cards and shuffle it
+    print("Deck before shuffling:")
     deck = DeckOfCards()
+    deck.print_deck()
+    print("\nDeck after shuffling:")
     deck.shuffle_deck()
+    deck.print_deck()
 
     # deal two cards to the user
     card = deck.get_card()
+    print(f"\n\nCard 1: {CYAN}{card}{RESET}")
     card2 = deck.get_card()
+    print(f"Card 2: {CYAN}{card2}{RESET}")
 
     score = 0
     # calculate the user's hand score
     score = score_keeper(score, card)
     score = score_keeper(score, card2)
-    print(f"\n{GREEN}Your starting score is:{RESET}", score)
+    print(f"{GREEN}Your starting score is:{RESET}", score)
 
     # user card dealing loop
     hit_number = 0
@@ -115,8 +123,8 @@ while True:
             print(f"{CYAN}{card3}{RESET}")
             print(f"{CYAN}new score:{RESET}", score)
         elif hit == 'n':
-            print(snark.stand())
-            print("\nyour final score is:", score)
+            print(f"{MAGENTA}{snark.stand()}{RESET}")
+            print(f"{CYAN}your final score is:{RESET}", score)
 
             # check if the game is over
             game_over, player_won = game_status_checker(score, hit_number)
@@ -137,12 +145,12 @@ while True:
     # ask if the player would like to go again
     go_again = play_again()
     if go_again and player_won:
-        print(snark.play_again_win())
+        print(f"{MAGENTA}{snark.play_again_win()}{RESET}")
     elif go_again and not player_won:
-        print(snark.play_again_lose())
+        print(f"{MAGENTA}{snark.play_again_lose()}{RESET}")
     elif not go_again and player_won:
-        print(snark.no_play_again_win())
+        print(f"{MAGENTA}{snark.no_play_again_win()}{RESET}")
         break
     elif not go_again and not player_won:
-        print(snark.no_play_again_lose())
+        print(f"{MAGENTA}{snark.no_play_again_lose()}{RESET}")
         break
